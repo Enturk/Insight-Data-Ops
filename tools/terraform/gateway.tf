ce0335d16094d60c4f7139d6c77a4ed1fab331e8
Enturk <nazimkaraca@gmail.com>
2018-09-25 21:22:36 +0000

    some terraform files
    	modified:   .gitignore
    	new file:   tools/terraform/.terraform/plugins/linux_amd64/lock.json
    	new file:   tools/terraform/.terraform/plugins/linux_amd64/terraform-provider-aws_v1.37.0_x4
    	new file:   tools/terraform/gateway.tf
    	new file:   tools/terraform/public.tf
    	new file:   tools/terraform/terraform.tfstate
    	new file:   tools/terraform/terraform.tfstate.backup
    	new file:   tools/terraform/vpc.tf

---
+++ tools/terraform/gateway.tf
@@ -0,0 +1,5 @@
+resource "aws_internet_gateway" "default" {
+vpc_id = "${aws_vpc.default.id}"                                                                                                                                       tags {
+Name = "terraform gw"
+}
+}
