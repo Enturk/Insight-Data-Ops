667694cab9018cb9271def000258da215c7c2c70
InsecureEyeBeam <nazimkaraca@gmail.com>
2018-10-02 13:41:14 +0000

    scraper_setup runs on instance startup

--- src/newTerraInst.sh
+++ src/newTerraInst.sh
@@ -13,46 +13,16 @@ echo 'creating instance...'
 terraform init
 terraform plan
 terraform apply -auto-approve
-ip=terraform output ip
+ip=$(terraform output ip)
 
-#echo "storing instance details..."
-# store the data
-#aws --profile $account --region $region ec2 describe-instances --instance-ids $id > instance-details.json
- 
-# SSH
-# wait from: https://stackoverflow.com/questions/11904772/how-to-create-a-loop-in-bash-that-is-waiting-for-a-webserver-to-respond
-echo "Waiting for instance at $ip to become responsive."
-until $(curl --output /dev/null --silent --head --fail $ip); do
-    printf '.'
-    sleep 5
-done
- echo "SSH-ing into instance at $ip to prepare and run script"
-ssh -i ~/.ssh/terraformer.pem ubuntu@ec2-54-85-128-241.compute-1.amazonaws.com
+# Running script once on instance: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
+# To run on restart, use this: https://aws.amazon.com/premiumsupport/knowledge-center/execute-user-data-ec2/
 
-# # Python from: https://askubuntu.com/questions/101591/how-do-i-install-the-latest-python-2-7-x-or-3-x-on-ubuntu
-# sudo apt-get install build-essential checkinstall
-# sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-# version=2.7.13
-# cd ~/Downloads/
-# wget https://www.python.org/ftp/python/$version/Python-$version.tgz
-# tar -xvf Python-$version.tgz
-# cd Python-$version
-# ./configure
-# make
-# sudo checkinstall
+ip_with_dashes= "$ip" | tr . -
+ssh_target=ubuntu@ec2-$ip.compute-1.amazonaws.com
 
-# # Dowload & run script
-# #TODO install git 
-# mkdir scrape #check path
-# cd scrape
-# git clone #url
-# python src/scraper
-
-# # Send logs to right place & logout
-# scpy #send to right folder
-# logout
-
-# echo #log
+echo "Copy & paste these three lines into one bash command line:"
+echo 'ssh -i ~/.ssh/terraformer.pem -o "StrictHostKeyChecking no" '$ssh_target
 
 # echo "Terminating instance with prejudice."
 # terraform destroy -auto-approve
---
+++ src/scraper_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+touch file.txt
+echo "Boo!" > file.txt
