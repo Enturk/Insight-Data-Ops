23b02a106d7db8d5fa1e524a61da5cad47b76387
Nazim <ubuntu@ip-172-31-40-33.ec2.internal>
2018-10-09 00:12:47 +0000

    airflow runing again, working on getting .env to instance

--- tools/terraform/main.tf
+++ tools/terraform/main.tf
@@ -5,18 +5,29 @@ resource "aws_instance" "pipe-instance" {
   user_data = "${file("~/Insight-Data-Ops/src/scraper_setup.sh")}"
 
   # Allow AWS infrastructure metadata to propagate.
-  #provisioner "local-exec" {
-  #  command = "sleep 15"
-  #}
+  provisioner "local-exec" {
+    command = "sleep 15"
+  }
+
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+
+    # The path to your keyfile
+    # private_key = "${file("~/.ssh/swilson-IAM-keypair.pem")}"
+
+    type = "ssh"
+    host = "${aws_instance.pipe-instance.public_ip}" 
+  }
 
 
- # provisioner "remote-exec" {
- #   inline = [
- #     "sudo echo 'export POSTGRES_HOST=${var.postgres_host}' >> $HOME/.profile",
- #     "sudo echo 'export POSTGRES_USER=${var.aws_secret_key}' >> $HOME/.profile",
- #     "sudo echo 'export POSTGRES_PASSWORD=${var.postgres_password}' >> $HOME/.profile",
- #   ]
- # }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo echo 'export POSTGRES_HOST=${var.postgres_host}' >> $HOME/.profile",
+      "sudo echo 'export POSTGRES_USER=${var.aws_secret_key}' >> $HOME/.profile",
+      "sudo echo 'export POSTGRES_PASSWORD=${var.postgres_password}' >> $HOME/.profile",
+    ]
+  }
 
   tags {
     Name = "pipe-instance"
